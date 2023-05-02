76004c4f637533b3e89d107fce7e7241399ff5e3
3026187875 <3026187875@qq.com>
2023-05-02 18:43:09 +0800

    库文件和头文件

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # A200
 A200+mini摄像头开发       
 使用A200+mini开发一个三维扫描仪
+# 文件说明
++ include 
+> 头文件
++ libs
+> 库文件
+> 把库添加到系统库路径, 免得每次运行需要指定库搜索路径
++ 55-iminect-usb.rules
+> 设备挂载规则
+> ```bash
+> chmod 777 55-iminect-usb.rules
+> sudo -S cp 55-iminect-usb.rules /etc/udev/rules.d
+> sudo -S udevadm control --reload-rules
+> ```
---
+++ src/main.cpp
@@ -0,0 +1,179 @@
+/********************************************************************************
+Copyright (c) 2015 Huajie IMI Technology Co., Ltd.
+All rights reserved.
+
+@File Name		: main.cpp
+@Author			: Wendell
+@Date			: 2018-06-18
+@Description	: read color frame and view
+@Version		: 1.0.0
+@History		:
+1.2018-06-18 Wendell Created file
+********************************************************************************/
+#include <cstdio>
+
+// Imi Head File
+#include "ImiNect.h"
+
+// UI
+#include "../Common/Render.h"
+
+// window handle
+SampleRender* g_pRender = nullptr;
+
+ImiDeviceAttribute* g_DeviceAttr = nullptr;
+
+// device handle
+ImiDeviceHandle g_ImiDevice = nullptr;
+
+// stream handles
+ImiStreamHandle g_streams[10];
+uint32_t g_streamNum = 0;
+
+// window callback, called by SampleRender::display()
+static bool needImage(void* pData)
+{
+	static RGB888Pixel s_rgbImage[1280 * 1024];
+
+    int32_t avStreamIndex = 0;
+	ImiImageFrame* imiFrame = nullptr;
+
+    // frame read.
+    if(0 != imiReadNextFrame(g_streams[avStreamIndex], &imiFrame, 100))
+	{
+		return false;
+	}
+
+	if(nullptr == imiFrame)
+	{
+		return false;
+	}
+
+    // show to the window
+	switch(imiFrame->pixelFormat)
+	{
+		case IMI_PIXEL_FORMAT_IMAGE_RGB24:
+		{
+			g_pRender->draw(-1, (uint8_t*)imiFrame->pData, imiFrame->size, imiFrame->width, imiFrame->height);
+			break;
+		}
+		case IMI_PIXEL_FORMAT_IMAGE_YUV420SP:
+		{
+			YUV420SPToRGB((uint8_t*)s_rgbImage, (uint8_t*)imiFrame->pData, imiFrame->width, imiFrame->height);
+			g_pRender->draw(-1, (uint8_t*)s_rgbImage, imiFrame->width * imiFrame->height * 3, imiFrame->width, imiFrame->height);
+			break;
+		}
+		case IMI_PIXEL_FORMAT_IMAGE_YUV422:
+		{
+			YUV422ToRGB((uint8_t*)s_rgbImage, (uint8_t*)imiFrame->pData, imiFrame->width, imiFrame->height);
+			g_pRender->draw(-1, (uint8_t*)s_rgbImage, imiFrame->width * imiFrame->height * 3, imiFrame->width, imiFrame->height);
+			break;
+		}
+	}
+
+    // call this to free frame 
+    imiReleaseFrame(&imiFrame);
+
+    return true;
+}
+
+int Exit() 
+{
+	//7.imiCloseStream()
+	for(uint32_t num = 0; num < g_streamNum; ++num) 
+	{
+		if(nullptr != g_streams[num])
+		{
+			imiCloseStream(g_streams[num]);
+			g_streams[num] = nullptr;
+		}
+	}
+
+	//8.imiCloseDevice()
+	if(nullptr != g_ImiDevice)
+	{
+		imiCloseDevice(g_ImiDevice);
+		g_ImiDevice = nullptr;
+	}
+
+	//9.imiReleaseDeviceList
+	if(nullptr != g_DeviceAttr)
+	{
+		imiReleaseDeviceList(&g_DeviceAttr);
+		g_DeviceAttr = nullptr;
+	}
+
+	//10.imiDestroy()
+	imiDestroy();
+
+	if(nullptr != g_pRender)
+	{
+		delete g_pRender;
+		g_pRender = nullptr;
+	}
+
+	printf("------ exit ------\n");
+
+	getchar();
+
+	return 0;
+}
+
+int main(int argc, char** argv)
+{
+	//1.imiInitialize()
+	int ret = imiInitialize();
+	if(0 != ret)
+	{
+		printf("ImiNect Init Failed! ret = %d\n", ret);
+		return Exit();
+	}
+	printf("ImiNect Init Success.\n");
+	
+	//2.imiGetDeviceList()
+	uint32_t deviceCount = 0;
+	imiGetDeviceList(&g_DeviceAttr, &deviceCount);
+	if((deviceCount <= 0) || (nullptr == g_DeviceAttr))
+	{
+		printf("Get No Connected ImiDevice!\n");
+		return Exit();
+	}
+	printf("Get %d Connected ImiDevice.\n", deviceCount);
+
+	//3.imiOpenDevice()
+	ret = imiOpenDevice(g_DeviceAttr[0].uri, &g_ImiDevice, 0);
+	if(0 != ret)
+	{
+		printf("Open ImiDevice Failed! ret = %d\n", ret);
+		return Exit();
+	}
+	printf("ImiDevice Opened.\n");
+
+	//4.imiSetFrameMode()
+    const ImiFrameMode* pFrameMode = imiGetCurrentFrameMode(g_ImiDevice, IMI_COLOR_FRAME);
+    if (nullptr == pFrameMode)
+    {
+        printf("Get current frame mode failed!\n");
+        return Exit();
+    }
+    printf("Get current frame mode:%d-%d-%d-%d-%d\n", pFrameMode->bitsPerPixel, pFrameMode->framerate,
+        pFrameMode->pixelFormat, pFrameMode->resolutionX, pFrameMode->resolutionY);
+    imiSetFrameMode(g_ImiDevice, IMI_COLOR_FRAME, (ImiFrameMode*)pFrameMode);
+
+	//5.imiOpenStream()
+	ret = imiOpenStream(g_ImiDevice, IMI_COLOR_FRAME, nullptr, nullptr, &g_streams[g_streamNum++]);
+	if(0 != ret)
+	{
+		printf("Open Color Stream Failed! ret = %d\n", ret);
+		return Exit();
+	}
+	printf("Open Color Stream Success.\n");
+
+    //6.create window and set read Stream frame data callback
+    g_pRender = new SampleRender("RGB Color View", pFrameMode->resolutionX, pFrameMode->resolutionY);  // window title & size
+    g_pRender->init(argc, argv);
+    g_pRender->setDataCallback(needImage, nullptr);
+    g_pRender->run();
+
+	return Exit();
+}
